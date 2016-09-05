b0afd62a2be3b50382f83bfbd1bad64a6cfbb516
John Andre Hestad <jah@blankoslo.no>
2016-09-05 15:42:36 +0200

    Let App import its own component

--- src/containers/app.js
+++ src/containers/app.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getProjects, getHoursPerProject } from '../actions/index';
 import periodSelector from '../selectors/periodSelector';
+import IndexComponent from '../components/index';
 
 class App extends Component {
   constructor(props) {
@@ -15,22 +16,14 @@ class App extends Component {
     if (this.props.projects.loading) {
       return null;
     }
-    const children = React.Children.map(this.props.children,
-      child => React.cloneElement(child, {
-        projects: this.props.projects,
-      }));
 
     return (
-      <div>
-        {children}
-      </div>
+      <IndexComponent projects={this.props.projects} />
     );
   }
 }
 
 App.propTypes = {
-  children: React.PropTypes.object,
-
   // mapStateToProps
   projects: React.PropTypes.object.isRequired,
   startDate: React.PropTypes.string.isRequired,
