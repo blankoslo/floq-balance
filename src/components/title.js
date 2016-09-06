f7c3ce52ff29b114f5f9ca93a4f7321b6ec698e5
John Andre Hestad <jah@blankoslo.no>
2016-09-06 16:38:11 +0200

    Add navigation shell
    Missing actual interaction.

--- src/components/title.js
+++ src/components/title.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const BalanceViewTitle = (props) => (
-  <div style={{ textAlign: 'center' }}>
-    <h2>{props.selectedMonth} {props.selectedYear}</h2>
+  <div className='balance-navigation'>
+    <Link
+      className='mdl-button mdl-js-button'
+      to={''}
+    >
+      <i className='material-icons'>arrow_back</i>
+    </Link>
+    <div style={{ textAlign: 'center' }}>
+      <h2>{props.selectedMonth} {props.selectedYear}</h2>
+    </div>
+    <Link
+      className='mdl-button mdl-js-button'
+      to={''}
+    >
+      <i className='material-icons'>arrow_forward</i>
+    </Link>
   </div>
 );
 
