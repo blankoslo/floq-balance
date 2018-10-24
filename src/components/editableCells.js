a8526ab90b69ec155be1ec74f72edc6f155dae4c
Ole Christer Selvig <ole.selvig@blank.no>
2018-10-24 11:34:35 +0200

    Modify editable cells to show proper formatting

--- src/components/editableCells.js
+++ src/components/editableCells.js
@@ -47,89 +47,174 @@ export const DurationStaticCell = ({ value, hours, decimals, onClick, className 
   const numDecimals = input => (input === undefined || input === null ? 1 : input);
   return (
     <div onClick={onClick} className={className}>
-      {Math.abs(value) < 0.1
-        ? ""
-        : hours
-          ? (value / 60).toFixed((value / 60) % 1 ? numDecimals(decimals) : 0)
-          : value}
+      {hours ? (value / 60).toFixed((value / 60) % 1 ? numDecimals(decimals) : 0) : value}
     </div>
   );
 };
 
-const EditableCell = ({ onBlurEventHandler, input }) => (
-  <div
-    style={{ flexGrow: "1", outline: "none", display: "flex", justifyContent: "flex-end" }}
-    contentEditable
-    suppressContentEditableWarning
-    onBlur={onBlurEventHandler}
-    dangerouslySetInnerHTML={{
-      __html: input
-    }}
-  />
-);
+class EditableCell extends React.Component {
+  constructor(props) {
+    super(props);
+    this.inputCell = React.createRef();
+  }
+
+  componentDidMount() {
+    const node = this.inputCell.current;
+    node.focus();
+    let range = document.createRange();
+    range.selectNodeContents(node);
+    range.collapse(false);
+    let selection = window.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
+  }
+
+  render() {
+    const { onBlurEventHandler, input } = this.props;
+    return (
+      <div
+        style={{ flexGrow: "1", outline: "none", display: "flex", justifyContent: "flex-end" }}
+        ref={this.inputCell}
+        contentEditable
+        suppressContentEditableWarning
+        onBlur={onBlurEventHandler}
+        dangerouslySetInnerHTML={{
+          __html: input
+        }}
+      />
+    );
+  }
+}
 
 export class FeeCell extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      hasActiveFocus: false
+    };
   }
 
   onBlurEventHandler = e => {
     const newValue = e.target.innerHTML.trim() === "" ? "0" : e.target.innerHTML;
     this.props.onInputChange(this.props.projectId, this.props.columnId, newValue);
+    this.setState({ hasActiveFocus: false });
     if (newValue === this.props.value.toString()) return;
     if (!isValidAmount(newValue)) return;
     this.props.onValueChange(this.props.projectId, this.props.billedMinutes, Number(newValue));
   };
 
+  setActiveFocus = () => {
+    this.setState({ hasActiveFocus: true });
+  };
+
   render() {
-    return <EditableCell input={this.props.input} onBlurEventHandler={this.onBlurEventHandler} />;
+    return (
+      <div onClick={this.setActiveFocus}>
+        {this.state.hasActiveFocus ? (
+          <EditableCell input={this.props.input} onBlurEventHandler={this.onBlurEventHandler} />
+        ) : (
+          <MonetaryStaticCell value={this.props.value} />
+        )}
+      </div>
+    );
   }
 }
 
 export class BilledHoursCell extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      hasActiveFocus: false
+    };
   }
   onBlurEventHandler = e => {
     const newValue = e.target.innerHTML.trim() === "" ? "0" : e.target.innerHTML;
     this.props.onInputChange(this.props.projectId, this.props.columnId, newValue);
+    this.setState({ hasActiveFocus: false });
     if (newValue === this.props.value.toString()) return;
     if (!isValidHours(newValue)) return;
     this.props.onValueChange(this.props.projectId, Number(newValue) * 60, this.props.fee);
   };
+
+  setActiveFocus = () => {
+    this.setState({ hasActiveFocus: true });
+  };
+
   render() {
-    return <EditableCell input={this.props.input} onBlurEventHandler={this.onBlurEventHandler} />;
+    return (
+      <div onClick={this.setActiveFocus}>
+        {this.state.hasActiveFocus ? (
+          <EditableCell input={this.props.input} onBlurEventHandler={this.onBlurEventHandler} />
+        ) : (
+          <DurationStaticCell value={this.props.value} hours={true} />
+        )}
+      </div>
+    );
   }
 }
 export class ExpenseCell extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      hasActiveFocus: false
+    };
   }
   onBlurEventHandler = e => {
     const newValue = e.target.innerHTML.trim() === "" ? "0" : e.target.innerHTML;
     this.props.onInputChange(this.props.projectId, this.props.columnId, newValue);
+    this.setState({ hasActiveFocus: false });
     if (newValue === this.props.value.toString()) return;
     if (!isValidAmount(newValue)) return;
     this.props.onValueChange(this.props.projectId, this.props.type, Number(newValue));
   };
+
+  setActiveFocus = () => {
+    this.setState({ hasActiveFocus: true });
+  };
+
   render() {
-    return <EditableCell input={this.props.input} onBlurEventHandler={this.onBlurEventHandler} />;
+    return (
+      <div onClick={this.setActiveFocus}>
+        {this.state.hasActiveFocus ? (
+          <EditableCell input={this.props.input} onBlurEventHandler={this.onBlurEventHandler} />
+        ) : (
+          <MonetaryStaticCell value={this.props.value} />
+        )}
+      </div>
+    );
   }
 }
+
 export class WriteOffCell extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      hasActiveFocus: false
+    };
   }
 
   onBlurEventHandler = e => {
     const newValue = e.target.innerHTML.trim() === "" ? "0" : e.target.innerHTML;
     this.props.onInputChange(this.props.projectId, this.props.columnId, newValue);
+    this.setState({ hasActiveFocus: false });
     if (this.props.value.toString() === newValue) return;
     if (!isValidHours(newValue)) return;
     this.props.onValueChange(this.props.projectId, Number(newValue) * 60);
   };
 
+  setActiveFocus = () => {
+    this.setState({ hasActiveFocus: true });
+  };
+
   render() {
-    return <EditableCell input={this.props.input} onBlurEventHandler={this.onBlurEventHandler} />;
+    return (
+      <div onClick={this.setActiveFocus}>
+        {this.state.hasActiveFocus ? (
+          <EditableCell input={this.props.input} onBlurEventHandler={this.onBlurEventHandler} />
+        ) : (
+          <DurationStaticCell value={this.props.value} hours={true} />
+        )}
+      </div>
+    );
   }
 }
